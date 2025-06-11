556939022937fa381ec218871425f14c888b9f16
Polarived <samuel.tomich@outlook.com>
2025-06-11 08:35:53 +0000

    it dont work

--- BinartFormart.py
+++ BinartFormart.py
@@ -1,21 +1,19 @@
 import random
 
-class Format():
-    def __init__(self):
-        pass
-    def bformat(self, rng: int):
-        num = random.randint(0,self.rng)
-        tries = 0
-        while True:
-            self.rng /= 2
-            tries += 1
-            if rng == num:
-                print(f"It took {tries}, tries")
-                print("Found the number!")
-                break
-            else:
-                print(f"Tries: {tries}")
+def bformat(rng: int):
+    num = random.randint(0,rng)
+    tries = 0
+    while True:
+        tries += 1
+        if rng == num:
+            print(f"It took {tries}, tries")
+            print("Found the number!")
+            break
+        elif num < rng:
+            rng /= 2
+        else:
+            print(f"Tries: {tries}")
 
 
-RAHH = Format()
-RAHH.bformat(1000000)
+
+bformat(1000000)
